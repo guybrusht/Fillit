b0bddcdd1c4a8a9a661b5e974b0870a16ec87ad0
Amine LALAOUI <alalaoui@e1r13p1.42.fr>
2017-05-10 20:23:09 +0200

    split coded

--- structure.c
+++ structure.c
@@ -6,7 +6,7 @@
 /*   By: alalaoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/09 17:32:21 by alalaoui          #+#    #+#             */
-/*   Updated: 2017/05/10 14:46:57 by alalaoui         ###   ########.fr       */
+/*   Updated: 2017/05/10 20:21:29 by alalaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,33 @@ int			ft_tetriminos_nb(char *ttris)
 		i++;
 	}
 	return (nb);
+}
+
+char		**ft_split_tmino(char *ttris)
+{
+	char	**ttristab;
+	int		tablen;
+	int		i;
+	char	*last;
+	char	*address;
+
+	last = ft_strlen(ttris) + ttris;
+	i = 0;
+	tablen = ft_tetriminos_nb(ttris);
+	printf("tablen:%d\n", tablen);
+	ttristab = (char**)malloc(sizeof(char*) * (tablen + 1));
+	while (i < tablen)
+	{
+		if ((address = ft_strstr(ttris, "\n\n")))
+			ttristab[i] = ft_strsub(ttris, 0, (address - ttris));
+		else
+			ttristab[i] = ft_strsub(ttris, 0, last - ttris);
+		ttris = address + 2;
+		printf("%d:\n%s\n", i, ttristab[i]);
+		i++;
+	}
+	ttristab[tablen] = NULL;
+	return (ttristab);
 }
 
 int			ft_analysis(char *ttris)
