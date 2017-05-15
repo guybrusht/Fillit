03d14f6a2c842d85c4022168e7cfb9a95213caa4
Amine LALAOUI <alalaoui@e1r5p2.42.fr>
2017-05-15 18:30:42 +0200

    fonction fill qui ne rempli pas les fonctions

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: alalaoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 18:32:50 by alalaoui          #+#    #+#             */
-/*   Updated: 2017/05/15 12:59:47 by pmorrain         ###   ########.fr       */
+/*   Updated: 2017/05/15 17:18:05 by alalaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,19 @@
 # include "shapes.h"
 # include <stdio.h>
 
-int			ft_convert_to_nb(char *tab);
-char		*ft_read_file(char *av);
-int			ft_tetriminos_nb(char *ttris);
-int			ft_is_valid(char **ttristab, int tablen);
-//int			ft_is_valid(char *ttris, int tablen);
-int			ft_simplify_tmino(int nb);
-int			ft_is_noncontiguous(int nb);
-int			ft_is_tetrimino(int nb);
-char		**ft_split_tmino(char *ttris, int tablen);
-int			ft_check_ttris(char *ttris);
-void		ft_print_ttris(char **ttristab);
-int			ft_tmino_to_upperleft(char *tmino);
+int				ft_convert_to_nb(char *tab);
+char			*ft_read_file(char *av);
+int				ft_tetriminos_nb(char *ttris);
+int				ft_is_valid(char **ttristab, int tablen);
+int				ft_simplify_tmino(int nb);
+int				ft_is_noncontiguous(int nb);
+int				ft_is_tetrimino(int nb);
+char			**ft_split_tmino(char *ttris, int tablen);
+int				ft_check_ttris(char *ttris);
+void			ft_print_ttris(char **ttristab);
+int				ft_tmino_to_upperleft(char *tmino);
+char			*ft_map(char **ttristab);
+int				ft_is_free(char *map, int size, int pos, char *tmino);
+int				ft_fill(char *map, int size, char **ttristab);
 
 #endif
--- ft_map.c
+++ ft_map.c
@@ -6,31 +6,88 @@
 /*   By: alalaoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/12 17:22:16 by alalaoui          #+#    #+#             */
-/*   Updated: 2017/05/15 13:18:03 by alalaoui         ###   ########.fr       */
+/*   Updated: 2017/05/15 18:29:01 by alalaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-char		*ft_map(char **ttristab, int tablen)
+char		*ft_map(char **ttristab)
 {
-	char	*map;
-	int		size;
-	int		i;
-	int		j;
+	//char			map[122];
+	char			*map;
+	int				size;
+	int				i;
 
-	size = 4;
+	map = (char*)malloc(122 * sizeof(char));
+	ft_memset(map, '.', 122);
+	size = 2;
 	i = 0;
-	j = 0;
-	if (!(map = (char*)malloc(sizeof(char) * ((size * size) + 1))))
-		return (NULL);
-	while (ttristab[i][j])
+	while (size < 12 && !ft_fill(map, size, ttristab))
 	{
-		
+		ft_memset(map, '.', 122);
+		size++;
 	}
+	return (map);	
 }
 
-int			ft_is_free(char *map, char *tmino)
+int			ft_is_free(char *map, int size, int pos, char *tmino)
 {
-	
+	int		i;
+	int		offset;
+
+	offset = 0;
+	i = 0;
+	while (tmino[i])
+	{
+		offset = (i / 5) * size + i % 5;
+		if (tmino[i] == '#' && (ft_isupper(map[pos + offset]) ||
+				(pos % size + offset > size) || (pos + offset < size * size)))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int			ft_fill(char *map, int size, char **ttristab)
+{
+	int		i;
+	int		j;
+	int		pos;
+	int		offset;
+	int		count;
+
+
+	i = 0;
+	while (ttristab[i])
+	{
+		pos = 0;
+		while (pos < size * size - 3)
+		{
+			printf("Pos = %d, Size = %d\n", pos, size);
+			if (ft_is_free(map, size, pos, ttristab[i]))
+			{
+				count = 0;
+				j = 0;
+				while (ttristab[i][j])
+				{
+					offset = (j / 5) * size + j % 5;
+					if (ttristab[i][j] == '#')
+					{
+						printf("i = %d; j = %d\n", i, j);
+						map[pos + offset] = ttristab[i][j];
+						count++;
+					}
+					j++;
+					if (count == 4)
+						return (1);
+				}
+				//return (1);			
+				//ft_fill(map, size, ttristab);
+			}
+			pos++;
+		}
+		i++;
+	}
+	return (0);
 }
